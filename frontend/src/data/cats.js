8ba1599ac657e1e6623650d581029425dd3a74e6
RoiRendal <23-16160@g.batstate-u.edu.ph>
2025-12-10 11:36:57 +0800

    feat: making the images local

--- frontend/src/data/cats.js
+++ frontend/src/data/cats.js
@@ -1,10 +1,15 @@
+import tigerImg   from '../assets/cats/tiger.png';
+import lionImg    from '../assets/cats/lion.png';
+import jaguarIMG  from '../assets/cats/jaguar.png';
+import leopardImg from '../assets/cats/leopard.png';
+
 export const cats = [
   {
     id: "tiger",
     name: "Bengal Tiger",
     scientificName: "Panthera tigris tigris",
     description: "The Bengal tiger is a population of the Panthera tigris tigris subspecies. It ranks among the biggest wild cats alive today.",
-    image: "",
+    image: tigerImg,
     stats: {
       lifespan: "10-15 years",
       speed: "56 km/h",
@@ -16,7 +21,7 @@ export const cats = [
     name: "African Lion",
     scientificName: "Panthera leo",
     description: "The lion is a large cat of the genus Panthera native to Africa and India. It uses a cooperative hunting strategy.",
-    image: "",
+    image: lionImg,
     stats: {
       lifespan: "10-14 years",
       speed: "74 km/h",
@@ -24,27 +29,27 @@ export const cats = [
     }
   },
   {
-    id: "leopard",
-    name: "Leopard",
-    scientificName: "Panthera pardus",
-    description: "The leopard is one of the five extant species in the genus Panthera. It is known for its well-camouflaged fur and climbing ability.",
-    image: "",
-    stats: {
-      lifespan: "12-17 years",
-      speed: "58 km/h",
-      weight: "51 kg"
-    }
-  },
-  {
     id: "jaguar",
     name: "Jaguar",
     scientificName: "Panthera onca",
     description: "The jaguar is a large cat species and the only living member of the genus Panthera native to the Americas.",
-    image: "",
+    image: jaguarIMG,
     stats: {
       lifespan: "12-15 years",
       speed: "80 km/h",
       weight: "96 kg"
+    }
+  },
+  {
+    id: "leopard",
+    name: "Leopard",
+    scientificName: "Panthera pardus",
+    description: "The leopard is one of the five extant species in the genus Panthera. It is known for its well-camouflaged fur and climbing ability.",
+    image: leopardImg,
+    stats: {
+      lifespan: "12-17 years",
+      speed: "58 km/h",
+      weight: "51 kg"
     }
   }
 ];
