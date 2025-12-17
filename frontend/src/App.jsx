abd64c8de177d6fd346b95008591ed2397a4778a
RoiRendal <23-16160@g.batstate-u.edu.ph>
2025-12-18 07:46:33 +0800

    style: changed some core UI design in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
@@ -8,17 +9,25 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="min-h-screen bg-stone-100 text-stone-800">
+    // CHANGE 1: Font is now 'font-serif'. Background is a classic 'bg-yellow-50' (paper feel).
+    // CHANGE 2: Added a 'border-x-4 border-black' container to make it look like a document.
+    <div className="min-h-screen bg-yellow-50 font-serif text-black selection:bg-black selection:text-white flex flex-col">
+      
       <Navbar />
-      <div className="p-6">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cat/:id" element={<CatDetail />} />
-          <Route path="/quiz" element={<Quiz />} />
-          <Route path="/add" element={<AddCat />} />
-          <Route path="/edit/:id" element={<AddCat />} />
-        </Routes>
+
+      {/* Main Content Area */}
+      <div className="flex-1 w-full max-w-7xl mx-auto border-x-0 md:border-x-4 border-black bg-white min-h-screen shadow-[8px_0_0_0_rgba(0,0,0,0.2)]">
+        <div className="p-4 md:p-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cat/:id" element={<CatDetail />} />
+            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/add" element={<AddCat />} />
+            <Route path="/edit/:id" element={<AddCat />} />
+          </Routes>
+        </div>
       </div>
+
       <Footer />
     </div>
   );
